9e9407cf1e531438d34c1c4bbead9033fc531e22
carryCode92 <chenyu@prodemacbook-pro.local>
2016-06-12 10:32:13 +0800

    Initial Commit

---
+++ tableVIewAndSegment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableVIewAndSegment
+//
+//  Created by chenyu on 16/6/12.
+//  Copyright © 2016年 chenyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableVIewAndSegmentTests/tableVIewAndSegmentTests.m
@@ -0,0 +1,39 @@
+//
+//  tableVIewAndSegmentTests.m
+//  tableVIewAndSegmentTests
+//
+//  Created by chenyu on 16/6/12.
+//  Copyright © 2016年 chenyu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tableVIewAndSegmentTests : XCTestCase
+
+@end
+
+@implementation tableVIewAndSegmentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
